8b1358b585ebfe1aec5c2c42ce9b9eeeccc71480
Roma <panochkor2105@gmail.com>
2024-12-11 00:54:05 +0200

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <BrowserRouter basename="/recipeapp"> 
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>,
+  document.getElementById('root')
+);
