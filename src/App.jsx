4f365fddf81a3d07d727cb8636c37e96d1c19c89
Roma <panochkor2105@gmail.com>
2024-12-11 01:01:07 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,6 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { CartProvider } from './Components/CartContent';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
-
 import Recipes from './Components/Recipes';
 import RecipeDetails from './Components/RecipeDetails';
 import BasketResipe from './Components/BasketResipe';
@@ -43,15 +42,13 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Recipes />} />
-            <Route path="/recipe/:id" element={<RecipeDetails />} />
-            <Route path="/basket" element={<BasketResipe />} />
-          </Routes>
-        </Router>
-      </CartProvider>
+ <CartProvider>
+  <Routes>
+    <Route path="/" element={<Recipes />} />
+    <Route path="/recipe/:id" element={<RecipeDetails />} />
+    <Route path="/basket" element={<BasketResipe />} />
+  </Routes>
+</CartProvider>
     </QueryClientProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,13 +9,14 @@
 // )
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom'; 
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter basename="/recipeapp"> 
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
